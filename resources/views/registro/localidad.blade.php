026fe2830e503c3b66d482d65864bd1e94e3c5e8
Ricardo Colin <david_ricardo12@hotmail.com>
2018-09-09 23:06:59 -0500

    Se corrigieron errores en registro, en consultas y se avanzo en ver registro (#42)

--- resources/views/registro/localidad.blade.php
+++ resources/views/registro/localidad.blade.php
@@ -3,11 +3,17 @@
   <h3 class="box-title col-sm-3">Localidad</h3>
   
   <label class="col-sm-3" style="white-space: nowrap; overflow: hidden;">
-    <input type="radio" name="Pais" id="Mexico" value="Mexico" onchange="changePais('forma');" checked>
+    <input type="radio" name="Pais" id="Mexico" value="Mexico" onchange="changePais('forma');" 
+        @if($datosGenerales['Pais'] != "Otro") checked 
+        @elseif($datosGenerales['Pais'] == "Otro" && $numeroLlamada > 0) disabled 
+        @endif >
     México
   </label>
   <label class="col-sm-3" style="white-space: nowrap; overflow: hidden;">
-    <input type="radio" name="Pais" value="Otro" onchange="changePais('forma');">
+    <input type="radio" name="Pais" value="Otro" onchange="changePais('forma');"
+        @if($datosGenerales['Pais'] == "Otro") checked 
+        @elseif($datosGenerales['Pais'] != "Otro" && $numeroLlamada > 0) disabled 
+        @endif >
     Otro
   </label>
 
@@ -22,15 +28,24 @@
 
     <div class="form-group col-md-4" style="white-space: nowrap;">
         <label for="CP">Codigo Postal:</label>
-        <input type="number" class="form-control" id="CP" name="CP" placeholder="#####">
+        <input type="number" 
+            class="form-control" 
+            id="CP" 
+            value="{{ $datosGenerales['CP'] }}"
+            @if($numeroLlamada > 0) disabled @endif
+            name="CP" 
+            placeholder="#####">
     </div>
 
     <div class="form-group col-md-8">
         <label for="Estado">Estado:</label>
-        <select name="Estado" id="Estado" class="form-control">
+        <select name="Estado"
+            id="Estado" 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control">
             <option value="0">-</option>
             @foreach ($estados as $k => $v)
-                <option value="{{$k}}">{{$v}}</option>
+                <option @if($datosGenerales['Estado'] == $v) selected @endif value="{{$k}}">{{$v}}</option>
             @endforeach
         </select>
     </div>
@@ -40,15 +55,27 @@
                style="white-space: nowrap; overflow: hidden; text-overflow: ellipsis;">
             Delegación o Municipio:
         </label>
-        <select name="Municipio" id="Municipio" class="form-control">
+        <select name="Municipio" 
+            id="Municipio" 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control">
             <option value="0">-</option>
+            @if($numeroLlamada > 0)
+                <option selected> {{ $datosGenerales['Municipio'] }} </option>
+            @endif
         </select>
     </div>
 
     <div class="form-group col-md-6">
         <label for="Colonia">Colonia:</label>
-        <select name="Colonia" id="Colonia" class="form-control">
+        <select name="Colonia" 
+            id="Colonia" 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control">
             <option value="0">-</option>
+            @if($numeroLlamada > 0)
+                <option selected> {{ $datosGenerales['Colonia'] }} </option>
+            @endif
         </select>
     </div>                                                                                    
 </div>
--- resources/views/registro/motivos.blade.php
+++ resources/views/registro/motivos.blade.php
@@ -11,60 +11,88 @@
     
     <div class="form-group col-md-12">
         <label for="AyudaPsicologico">PSICOLOGICO:</label>
-        <select name="AyudaPsicologico" id="AyudaPsicologico" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="AyudaPsicologico" 
+            id="AyudaPsicologico" 
+            multiple 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($mpsicologicos as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['AyudaPsicologico'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>
 
     <div class="form-group col-md-12">
         <label for="AyudaLegal">LEGAL:</label>
-        <select name="AyudaLegal" id="AyudaLegal" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="AyudaLegal" 
+            id="AyudaLegal" 
+            multiple 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($mlegales as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['AyudaLegal'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>
 
     <div class="form-group col-md-12">
         <label for="AyudaMedica">MEDICO:</label>
-        <select name="AyudaMedica" id="AyudaMedica" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="AyudaMedica" 
+            id="AyudaMedica" 
+            multiple 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($mMed as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['AyudaMedica'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>                          
 
     <div class="form-group col-md-12">
         <label for="AyudaOtros">OTROS:</label>
-        <select name="AyudaOtros" id="AyudaOtros" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="AyudaOtros" 
+            id="AyudaOtros" 
+            multiple 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($mOtr as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['AyudaOtros'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>
 
     <div class="form-group col-md-12">
         <label for="TipoViolencia">TIPO DE VIOLENCIA:</label>
-        <select name="TipoViolencia" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="TipoViolencia" 
+            @if($numeroLlamada > 0) disabled @endif
+            multiple 
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($tv as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['TipoViolencia'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>
 
     <div class="form-group col-md-12">
         <label for="ModalidadViolencia">MODALIDAD DE VIOLENCIA:</label>
-        <select name="ModalidadViolencia" multiple class="form-control js-example-basic-multiple">
+        <select 
+            name="ModalidadViolencia" 
+            multiple 
+            @if($numeroLlamada > 0) disabled @endif
+            class="form-control js-example-basic-multiple minimum">
             <option value="0">-</option>
             @foreach ($mv as $v)
-                <option value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
+                <option @if(in_array($v['Nombre'],$datosGenerales['ModalidadViolencia'])) selected @endif value="{{$v['Nombre']}}">{{$v['Nombre']}}</option>
             @endforeach
         </select>
     </div>
