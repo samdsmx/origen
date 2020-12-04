97a2c1dd1cb5e415874784c96c3f9c0bd36bbd6a
Sergio Marquez <samdsmx@hotmail.com>
2020-12-04 04:13:26 -0800

    update graficas

--- Funciones/grafico_bar.php
+++ Funciones/grafico_bar.php
@@ -1,35 +1,28 @@
 <?
 	include ("../graficos/jpgraph.php");
 	include ("../graficos/jpgraph_bar.php");
-
-	$l1datay = array();
-	$l1datay = split(",",$datagx);
-	$l2datay = array();
-	$l2datay = split(",",$datagx2);
+	
+	$data1y = array();
+	$data1y = split(",",$datagx);
 	$datas=array();
 	$datas=split(",",$datax);
 
-		// Create the graph. 
-		$graph = new Graph(600,400,"auto");	
-		$graph->SetScale("textlin");
-		$graph->SetMargin(90,40,20,160);
-		$graph->SetShadow();
-
-		$graph->SetBackgroundGradient('white','white');
-		$graph->xaxis->SetTickLabels($datas);
-		
-		if(count($l1datay)>0){
-			$l1plot=new BarPlot($l1datay);
-			$l1plot->SetFillColor("blue");
-			$l1plot->SetWeight(1);
-			}
-		
-		$graph->Add($l1plot);
-			
-		$graph->title->SetFont(FF_FONT1,FS_BOLD);
-		$graph->yaxis->title->SetFont(FF_FONT1,FS_BOLD);
-		$graph->xaxis->SetFont(FF_ARIAL,FS_NORMAL,6);
-		$graph->xaxis->SetLabelAngle(45);
-		
+	// Create the graph. These two calls are always required
+	$graph = new Graph(600,400,"auto");	
+	$graph->SetScale("textlin");
+	
+	$theme_class=new UniversalTheme;
+	$graph->SetTheme($theme_class);
+	
+	$graph->SetMargin(90,40,20,160);
+	$graph->xaxis->SetTickLabels($datas);
+	$graph->xaxis->SetLabelAngle(45);
+	
+	// Create the bar plots
+	if(count($data1y)>0){
+		$b1plot = new BarPlot($data1y);
+		$b1plot->SetFillColor("blue");
+		$graph->Add($b1plot);
 		$graph->Stroke();
+	}
 ?>
